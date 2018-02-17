33a33f2fc65f0fa084e90e1953b7fa67ee5afd32
OGUZ VURUSKANER <e2172120@cenginek>
2018-02-17 18:01:04 +0300

    firstdraft

---
+++ pizza.py
@@ -0,0 +1,108 @@
+class slice():
+
+	def init(self,solust,sagalt,grid):
+		self.solust = solust
+		self.sagalt = sagalt
+		self.mushroomCount = 0
+		self.tomatoCount = 0
+		self.grid = grid
+	"""
+
+
+	
+
+	"""
+	def move(self,direction:str):
+		pass
+
+	
+
+
+	def checkMinDist(self,direction:str):
+		pass
+
+
+	# @returns {direction:str,distance:int}
+	def checkAllDistances(self)
+	
+		pass
+
+
+
+
+	
+
+class  grid():
+	
+	def __init__(self):
+	
+		self.board = None
+		self.minSliceByB = None
+		self.maxSliceByA = None
+		self.remainingT = None
+		self.remainingM = None
+		self.slices = []
+
+
+		
+		
+
+
+
+	def populateGrid(self):
+
+		
+		pass
+
+
+
+
+	def isTaken(self,coord) -> bool:
+		pass
+
+
+
+
+
+	def reduceTNumber(self,count):
+
+		pass
+
+
+	def reduceMNumber(self,count):
+
+		pass
+
+
+
+	
+	def placeNewSlice(self,coord ) -> slice:
+
+
+		pass
+
+
+
+
+	def getNewSlicePositionByDistance(self):
+		
+		pass
+
+
+
+	
+	
+	
+
+
+	def placeNSlice(self,sliceCount):
+		
+		pass
+
+	 	#iff board is empty
+
+		
+		
+
+
+	
