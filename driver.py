a9c36c1cd4ece6edf0bab02f20d1ca2d2379be9e
OGUZ VURUSKANER <e2172120@cenginek>
2018-02-17 19:34:42 +0300

    grid.py iki metodu uygulandı.

--- driver.py
+++ driver.py
@@ -17,14 +17,4 @@ if __name__ == "__main__":
 	#En az kalan eleman 
 	minority = "M" if table.remainingM <= table.remainingT else "T"
 	firstMinority = None
-	for i in range(table.r):
-		for j in range(table.c):	
-			if table[i][j][0] == minority:
-				firstMinority = table[i][j]
-				break;
-		"""____________________________________"""	
-		else:
-			continue
-
-		break
 
