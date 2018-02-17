b417d673a0a27a6a894da464b0526af7a42199fa
DENIZ RASIM ULUG <e2172088@ceng.metu.edu.tr>
2018-02-17 19:42:39 +0000

    update driver.py2

--- driver.py
+++ driver.py
@@ -2,6 +2,62 @@ from pizza import grid,slice
 
 
 
+#create table
+table = grid()
+
+#get how many slice to place
+slice_count_to_place = table.getSliceCountToPlace()
+
+#place first slice
+grid.placeFirstSlice()
+slice_count_to_place -= 1
+
+#place rest of slices
+for i in range(slice_count_to_place):
+	table.placeNewSlice()
+
+
+any_move_taken = False
+any_slice_removed = False
+any_slice_added = False
+
+while True:
+	any_move_taken = False
+	any_slice_removed = False
+	any_slice_added = False
+	#ASSUMPTION: one move per slice per iteration
+	for s in table.getOrderedSliceArray():
+		legal_move_tuple = s.getLegalMoves()
+		if(legal_move_tuple != (0,0,0,0) ): #decide on the format of legal_move_tuple 0,0,0,0 or [u,p,l]
+			direction_to_go = s.getDirection(legal_move_tuple)
+			s.move(direction_to_go)
+			any_move_taken = True
+
+	if (any_move_taken==False and table.getCurrentSliceCount()-1 > table.getMinSliceCount() ):
+		table.removeSlice()
+		any_slice_removed=True
+	
+	#remove slicedan sonra, hemen add mi yapsın, yoksa iterasyonda devam edip
+	# removelayamadığında mı add yapsın???
+	#
+	#su an ikincisini sectik
+	
+	if(any_slice_removed==False and table.getCurrentSliceCount()+1 < table.getMaxSliceCount()):
+		table.addSlice()
+		any_slice_added = True
+
+
+
+
+	if (any_move_taken or any_slice_removed or any_slice_added) == False:
+		break
+
+
+#now output the final values
+
+
+
+
 
 
 
@@ -12,19 +68,7 @@ from pizza import grid,slice
 
 
 if __name__ == "__main__":
-	
-	table = grid()
-	#En az kalan eleman 
-	minority = "M" if table.remainingM <= table.remainingT else "T"
-	firstMinority = None
-	for i in range(table.r):
-		for j in range(table.c):	
-			if table[i][j][0] == minority:
-				firstMinority = table[i][j]
-				break;
-		"""____________________________________"""	
-		else:
-			continue
+	main()
 
-		break
+
 
