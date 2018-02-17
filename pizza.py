df978dbb8c5e43fb4e73467b4075477e68794549
ONUR CAN TIRTIR <e2099380@cenginek>
2018-02-17 18:30:17 +0300

    populate pizza class

--- pizza.py
+++ pizza.py
@@ -1,108 +1,93 @@
-class slice():
-
-	def init(self,solust,sagalt,grid):
-		self.solust = solust
-		self.sagalt = sagalt
-		self.mushroomCount = 0
-		self.tomatoCount = 0
-		self.grid = grid
-	"""
-
-
+class slice:
+    def __init__(self, solust, sagalt, grid):
+	self.solust = solust
+	self.sagalt = sagalt
+	self.mushroomCount = 0
+	self.tomatoCount = 0
+	self.grid = grid
 	
-
-	"""
-	def move(self,direction:str):
-		pass
+    def move(self,direction:str):
+	pass
 
 	
 
 
-	def checkMinDist(self,direction:str):
-		pass
+    def checkMinDist(self,direction:str):
+	pass
 
 
 	# @returns {direction:str,distance:int}
-	def checkAllDistances(self)
+    def checkAllDistances(self)
 	
-		pass
+	pass
 
-
-
-
-	
-
-class  grid():
-	
-	def __init__(self):
-	
-		self.board = None
-		self.minSliceByB = None
-		self.maxSliceByA = None
-		self.remainingT = None
-		self.remainingM = None
-		self.slices = []
-
-
-		
-		
-
-
-
-	def populateGrid(self):
-
-		
-		pass
-
-
-
-
-	def isTaken(self,coord) -> bool:
-		pass
-
-
-
-
-
-	def reduceTNumber(self,count):
-
-		pass
-
-
-	def reduceMNumber(self,count):
-
-		pass
 
 
 
 	
-	def placeNewSlice(self,coord ) -> slice:
 
+from math import ceil
 
-		pass
-
-
-
-
-	def getNewSlicePositionByDistance(self):
-		
-		pass
-
-
-
+class grid():
 	
+    def __init__(self):
+	self.r, self.c, self.a, self.b = input().split()
+        # 2D arr of (chr, bool)
+        self.board = []
+        
+        self.mantarCount = self.domatesCount = 0
+        for i in range(self.r):
+            row = map(lambda x: (x, False), input().split())
+            self.board.append(row)
+            for (c, _) in row:
+                if c == 'M':
+                    self.mantarCount += 1
+                else:
+                    self.domatesCount += 1
+
+        
+        # TODO WARNING
+        if self.mantarCount > self.domatesCount:
+            self.mantarCount, self.domatesCount = self.domatesCount, self.mantarCount
+            self.mantar  = 'T'
+            self.domates = 'M'
+    
+        else:
+            self.mantar  = 'M'
+            self.domates = 'T'
+
+        self.minSliceByB = int(ceil(r * c / b))
+	self.maxSliceByA = self.mantarCount // a
+
+        """
+        self.remainingT = None
+	self.remainingM = None
+        """
+
+	self.slices = []
+
+    """
+    def populateGrid(self):
+	pass
+    """
+
+    def isTaken(self, coord) -> bool:
+        return board[coord[0]][coord[1]]
+
+    def reduceTNumber(self, count):
+        self.domatesCount -= count
+
+    def reduceMNumber(self, count):
+        self.mantarCount -= count
 	
+    def placeNewSlice(self, coord) -> slice:
+        # TODO
+
+    def getNewSlicePositionByDistance(self):
+	# TODO
+
+    def placeNSlice(self,sliceCount):
+        for i in range(sliceCount):
+            self.placeNewSlice()
 	
 
-
-	def placeNSlice(self,sliceCount):
-		
-		pass
-
-	 	#iff board is empty
-
-		
-		
-
-
-	
